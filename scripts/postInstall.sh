957d6968c27a0abdb65f87b6957fe1d65de8cb0c
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-12-06 11:26:23 +0200

    fix

---
+++ scripts/postInstall.sh
@@ -0,0 +1,45 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#wait until the server is ready
+echo "Waiting for software to be ready ..."
+sleep 60s;
+
+
+
+
+# Set admin token process
+target=$(docker-compose port directus 8055)
+
+
+login=$(curl  -X POST \
+  http://${target}/auth/login \
+  --header 'Accept: */*' \
+  --header 'Content-Type: application/json' \
+  --data-raw '{
+	"email": "'${ADMIN_EMAIL}'",
+	"password": "'${ADMIN_PASSWORD}'"
+}')
+
+access_token=$(echo $login | jq -r '.data.access_token')
+
+
+users=$(curl  -X GET \
+  http://${target}/users \
+  --header 'Accept: */*' \
+  --header 'Authorization: Bearer '${access_token}'')
+
+userID=$(echo $users | jq -r '.data[0].id')
+
+
+curl  -X PATCH \
+  http://${target}/users/${userID} \
+  --header 'Accept: */*' \
+  --header 'Authorization: Bearer '${access_token}'' \
+  --header 'Content-Type: application/json' \
+  --data-raw '{
+  "token":"'${ADMIN_PASSWORD}'"
+}'
+
+
+expect ./scripts/expect.sh
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,3 +1,6 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
 mkdir -p ./uploads;
 chmod 777 ./uploads;
 
@@ -9,3 +12,33 @@ chmod 777 ./extensions/displays;
 
 mkdir -p ./data;
 chmod 777 ./data;
+
+# Install nodejs
+curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt install nodejs
+
+apt install jq -y
+apt-get install expect
+
+cat <<EOT > ./scripts/expect.sh
+#!/usr/bin/env expect
+
+spawn npx directus-template-cli@latest apply
+
+expect "Ok to proceed? (y)" { send "y\r" }
+
+expect "What type of template would you like to apply?" { send "1\r" }
+
+expect "Select a template." { send "1\r" }
+
+expect "What is your Directus URL?" { send "https://${DOMAIN}\r" }
+
+expect "What is your Directus Admin Token?" { send "${ADMIN_PASSWORD}\r" }
+
+interact
+
+EOT
+
+
+chmod +x ./scripts/expect.sh
